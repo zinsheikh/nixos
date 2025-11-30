5b249935242a02495d17ca16cefbe6a079feac0d
penguin <nixos@gays.eu>
2025-11-30 11:34:54 +0100

    ahhhhh i need to re enable gnome so i can actually work on this
     On branch master
    Changes to be committed:
    	modified:   configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -46,11 +46,11 @@
   };
 
   # Enable the X11 windowing system.
-  # services.xserver.enable = true;
+   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  # services.xserver.displayManager.gdm.enable = true;
-  # services.xserver.desktopManager.gnome.enable = true;
+   services.xserver.displayManager.gdm.enable = true;
+   services.xserver.desktopManager.gnome.enable = true;
 
   #attempting to enable niri
   # programs.niri.enable = true;
