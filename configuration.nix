04167a40226d81882aa32034f1eed9b7cb17749f
penguin <nixos@gays.eu>
2025-11-30 15:28:21 +0100

    after many attempts i now have a noctalaia flake that builds
    On branch master
     Changes to be committed:
    	modified:   configuration.nix
    	modified:   flake.lock
    	modified:   flake.nix

--- configuration.nix
+++ configuration.nix
@@ -51,8 +51,8 @@
    services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-   services.displayManager.gdm.enable = true;
-   services.desktopManager.gnome.enable = true;
+   services.xserver.displayManager.gdm.enable = true;
+   services.xserver.desktopManager.gnome.enable = true;
 
   #attempting to enable niri
   programs.niri.enable = true;
--- flake.nix
+++ flake.nix
@@ -1,17 +1,19 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     niri.url = "github:sodiboo/niri-flake";
 
     noctalia = {
       url = "github:noctalia-dev/noctalia-shell";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
   };
   outputs = inputs@{ self, nixpkgs, niri, ... }: {
     # replace 'joes-desktop' with your hostname here.
     nixosConfigurations.penguin = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
+      specialArgs = {inherit inputs;};
       modules = [ ./configuration.nix ./noctalia.nix];
     };
   };
