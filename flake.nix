962b6e7978a19b041adf9f5066b858b75a041102
penguin <nixos@gays.eu>
2025-12-02 20:52:18 +0100

    enabling home manager via flakes hopefully
     On branch master
     Changes to be committed:
    	modified:   flake.nix

--- flake.nix
+++ flake.nix
@@ -6,6 +6,9 @@
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
    # niri.url = "github:sodiboo/niri-flake";
 
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
     noctalia = {
       url = "github:noctalia-dev/noctalia-shell";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
@@ -17,12 +20,14 @@
     };
 
   };
-  outputs = inputs@{ 
-   self, 
-   nixpkgs,
-   nixpkgs-unstable,
-   #niri,
-  ... }: let
+   outputs = inputs@{ 
+     self, 
+     nixpkgs,
+     nixpkgs-unstable,
+   # niri,
+     home-manager,
+    ... }: 
+    let
     system = "x86_64-linux";
     unstable = import nixpkgs-unstable {
         inherit system;
@@ -33,7 +38,21 @@
     # replace 'joes-desktop' with your hostname here.
     nixosConfigurations.penguin = nixpkgs.lib.nixosSystem {
       specialArgs = {inherit inputs; inherit unstable;};
-      modules = [ ./configuration.nix ./noctalia.nix];
+      modules = [ 
+	./configuration.nix 
+	./noctalia.nix
+
+	 home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.penguin = ./home.nix;
+
+            # Optionally, use home-manager.extraSpecialArgs to pass
+            # arguments to home.nix
+          }
+
+      ];
     };
   };
 }
